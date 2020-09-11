38d27b3c0194ff2734edcaccaa79dbbf556a8cd8
Golovushkin <golovushkin_aa@mail.ru>
2020-09-11 18:55:55 +0400

    Исправление ошибок в HelloWorld

--- src/main/java/ru/ssau/tk/golovushkin532/OOP_PRACTICE/HelloWorld.java
+++ src/main/java/ru/ssau/tk/golovushkin532/OOP_PRACTICE/HelloWorld.java
@@ -1,9 +1,9 @@
 package ru.ssau.tk.golovushkin532.OOP_PRACTICE;
 
-public class HelloWorld
+public class HelloWorld {
 
-{
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
+
 }
