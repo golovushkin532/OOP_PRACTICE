a7ad4050f3cf412c112beef79504331f52f8c1ed
Golovushkin <golovushkin_aa@mail.ru>
2020-10-05 21:06:28 +0400

    Задание 1.3 (недоделал)

---
+++ src/main/java/ru/ssau/tk/golovushkin532/OOP_PRACTICE/Task_1/Point.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.golovushkin532.OOP_PRACTICE.Task_1;
+
+class Point {
+    public final double x;
+    public final double y;
+    public final double z;
+
+    Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+    }
+}
