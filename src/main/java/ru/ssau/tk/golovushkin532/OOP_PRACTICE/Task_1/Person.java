3b2a7cc9e39565ede5de90f1888341a1f7e470ee
Golovushkin <golovushkin_aa@mail.ru>
2020-10-05 15:06:00 +0400

    Задание 1.1

---
+++ src/main/java/ru/ssau/tk/golovushkin532/OOP_PRACTICE/Task_1/Person.java
@@ -0,0 +1,31 @@
+package ru.ssau.tk.golovushkin532.OOP_PRACTICE.Task_1;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int passportId;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPassportId(int passportId) {
+        this.passportId = passportId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getPassportId() {
+        return passportId;
+    }
+}
