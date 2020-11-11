cbfc1a1f72fc2db5ada45eb71c6b19a6370622be
Golovushkin <golovushkin_aa@mail.ru>
2020-11-11 21:19:37 +0400

    Задание 1.3

--- src/main/java/ru/ssau/tk/golovushkin532/OOP_PRACTICE/Task_1/Point.java
+++ src/main/java/ru/ssau/tk/golovushkin532/OOP_PRACTICE/Task_1/Point.java
@@ -5,10 +5,25 @@ class Point {
     public final double y;
     public final double z;
 
-    Point(double x, double y, double z) {
+    public Point(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
 
+    @Override
+    public String toString() {
+        return ("(" + x + ", " + y + ", " + z + ")");
+    }
+
+
+    public static void main(String[] args) {
+        Point onePoint = new Point(4, 8, 1);
+        Point secondPoint = new Point(7, 14, 2);
+        Point thirdPoint = new Point(-5, -6, -7);
+
+        System.out.println(onePoint);
+        System.out.println(secondPoint);
+        System.out.println(thirdPoint);
     }
 }
