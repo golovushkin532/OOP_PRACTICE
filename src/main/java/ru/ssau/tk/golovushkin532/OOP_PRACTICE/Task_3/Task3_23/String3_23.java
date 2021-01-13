aa680c981aa0e1896e2cb9efcd26926cf0e6b8ea
Golovushkin <golovushkin_aa@mail.ru>
2021-01-13 14:30:07 +0400

    3.23 забыл

---
+++ src/main/java/ru/ssau/tk/golovushkin532/OOP_PRACTICE/Task_3/Task3_23/String3_23.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.golovushkin532.OOP_PRACTICE.Task_3.Task3_23;
+
+import java.nio.charset.Charset;
+
+public class String3_23 {
+    public static void main(String[] args) {
+        System.out.println(Charset.defaultCharset());
+    }
+}
